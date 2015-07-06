9bd255103a7c9c49ef004b66c7b77f99d95ab74b
Purnima Gupta <pooyax14@gmail.com>
2015-07-07 04:05:33 +0530

    add content into div for improved rendering
    somehow, any content added into the div -- a &nbsp; in this case
    -- causes it to render properly as expected without any gaps in
    between rows and columns (the div.gridel elements have no margins
    or padding to space them out)

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     for(var i = 1; i<=16; i++){
         for(var j = 1; j<=16; j++){
             var div = document.createElement("div");
+            $(div).html("&nbsp;");
             $(div).addClass("gridel");
             $(div).on("mouseleave", function() {
                 $(this).addClass("painted");
