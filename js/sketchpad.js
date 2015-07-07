914aa6a2085d638beb901226bf3fdee2bd266e60
Purnima Gupta <pooyax14@gmail.com>
2015-07-07 19:00:21 +0530

    make the grid size dynamic based on user input

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -15,8 +15,36 @@ $(document).ready(function() {
     $(clear_button).text("Clear");
     $(clear_button).click(function() {
         $("div.gridel").removeClass("painted");
-    })
+        var num_boxes = prompt("Enter the number between 1 to 50");
+        num_boxes = parseInt(num_boxes);
+        draw_grid(num_boxes);
+    });
     $(clear_button).addClass("centeredLargeButton");
     $("body").append(clear_button);
+
+    function clear_old_grid() {
+        $(".container").html("");
+    }
+
+    // var ask_user = prompt("Enter the number between 1 to 16");
+    function draw_grid(number) {
+        clear_old_grid();
+        for(var i = 1; i<= number; i++) {
+            for(var j =1 ; j<=number; j++) {
+                var div = document.createElement("div");
+                $(div).html("&nbsp;");
+
+                $(div)
+                    .width(100/number + "%")
+                    .height(100/number + "%")
+                    .addClass("gridel")
+                    .on("mouseenter", function() {
+                        $(this).addClass("painted");
+                    });
+
+                $(".container").append($(div));
+            }
+        }
+    }
 });
 
