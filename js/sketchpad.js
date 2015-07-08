eaaf1c0b6801fdfc5e5f1f42f022809fa03d8f97
Purnima Gupta <pooyax14@gmail.com>
2015-07-09 00:21:03 +0530

    add utility functions
    - isArray() - tests whether input is an array
    - createButton() - creates a button and appends it to DOM

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,15 +1,99 @@
 $(document).ready(function() {
-        for(var i = 1; i<=16; i++) {
-            for(var j = 1; j<=16; j++) {
-                var div = document.createElement("div");
-                $(div).html("&nbsp;");
-                $(div).addClass("gridel");
-                $(div).on("mouseleave", function() {
-                    $(this).addClass("painted");
-                });
-                $(".container").append($(div));
+    for(var i = 1; i<=16; i++) {
+        for(var j = 1; j<=16; j++) {
+            var div = document.createElement("div");
+            $(div).html("&nbsp;");
+            $(div).addClass("gridel");
+            $(div).on("mouseleave", function() {
+                $(this).addClass("painted");
+            });
+            $(".container").append($(div));
+        }
+    }
+
+    /*
+    * isArray(that_thing):
+    *
+    * checks whether given JS variable is an array
+    *
+    * @that_thing: arbitrary variable/object to check
+    *
+    * returns: Boolean (true|false)
+    */
+    function isArray(that_thing) {
+        if(Object.prototype.toString.call( that_thing ) === "[object Array]") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    * createButton(caption, classname, parent):
+    *
+    * creates a button object, populates its text caption;
+    * assigns it a css class if given, and...
+    *
+    * @caption (String): the text in the button (required);
+    * @parent (String): css selector or JS reference of the intended parent element;
+    * @classname (String, or Array of strings): the css class to assign (optional);
+    *
+    * returns: reference to the newly created button object (not jquery object)
+    *
+    */
+    function createButton(caption, classname, parent) {
+        var button = document.createElement("button");
+
+        if( typeof caption !== "string") {
+            console.error("Could not create button object; invalid caption supplied.");
+            // add a default caption anyway
+            $(button).text("NO TEXT SUPPLIED!");
+        } else {
+            $(button).text(caption);
+        }
+
+        // if no parent supplied, append new button to body
+        // these checks are not sufficient but will work for now
+        if( typeof parent === "undefined" || parent === null) {
+            console.info("createButton() -> Info: no parent supplied" +
+                    "New button will be appended to BODY by default");
+            parent = "body";
+        }
+
+        // to catch another possible genuine mistakes
+        if (typeof parent == "number" || typeof parent === "") {
+            console.error("createButton() -> Error: invalid parent supplied" +
+                    parent +
+                    " is not valid html element to append to. " +
+                    "New button will be appended to BODY");
+            parent = "body";
+        }
+
+        if(!isArray(classname)) {
+            if(typeof classname === "string") {
+                // it could be either "foo bar baz"
+                // or "foo"
+                document.new_array = classname.split(" ");
+                classname = document.new_array;
+                delete document.new_array;
+            } else {
+                // print error
+                console.error("createButton() -> Error: classname should be " +
+                        "either a String or an Array of strings");
+            }
+
+            // apply the classes
+            for(var index in classname) {
+                $(button).addClass(classname[index]);
             }
         }
+
+        $(parent).append( $(button) );
+
+        return button;
+    }
+
+
     var grid_lines = document.createElement("button");
     $(grid_lines).text("show lines");
     $(grid_lines).addClass("centeredLargeButton").addClass("position");
